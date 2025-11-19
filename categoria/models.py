47466b3e6f381059d1f9648e70bdc7e91abc0311
LsCarlos01 <lscarlosfilho@gmail.com>
2025-11-18 21:58:25 -0300

    feat: :sparkles: adiciona models Categoria e Transacao

--- categoria/models.py
+++ categoria/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    tipo = models.CharField(
+        max_length=7,
+        choices=[
+            ('entrada', 'Entrada'),
+            ('saida', 'Saída'),
+        ]
+    )
+
+    def __str__(self):
+        return f"{self.nome} ({self.tipo})"
--- transacoes/models.py
+++ transacoes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from categoria.models import Categoria
 
 # Create your models here.
+class Transacoes(models.Model):
+    valor= models.DecimalField(max_digits=10, decimal_places=2)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    data= models.DateField()
+    
+    def __str__(self):
+        return f"{self.tipo} - {self.valor} ({self.categoria})" 
