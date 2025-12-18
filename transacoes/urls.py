de2d2b4ed281e7c09f4bdb09a04b10f05fecd6d4
LsCarlos01 <lscarlosfilho@gmail.com>
2025-12-18 10:27:37 -0300

    style: Funcionalidade de edição de transações e estlização das paginas(listar, edit)

--- transacoes/urls.py
+++ transacoes/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', listar_trasacoes, name='listar_transacoes'),
     path('cadastrar/', adicionar_transacao, name='adicionar_transacao'),
-    path('transacoes/<int:pk>/delete', transacoes_delete, name='transacoes_delete')
+    path('transacoes/<int:pk>/delete', transacoes_delete, name='transacoes_delete'),
+    path('transacoes/<int:pk>/edit', edit_transacao, name='edit_transacao')
 ]
--- transacoes/views.py
+++ transacoes/views.py
@@ -1,21 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Transacoes
 from .forms import TransacoesForm
+from categoria.models import Categoria
 
 
 def listar_trasacoes(request):
     transacoes = Transacoes.objects.all()
     return render(request, 'transacoes/listar_transacoes.html', {'transacoes': transacoes})
 
-def adicionar_transacao(request):  
+def adicionar_transacao(request):
+    categoria_id= request.GET.get('categoria')
+    if categoria_id:
+        categoria= Categoria.objects.filter(id=categoria_id).first()
+    else:
+        categoria= None
+
     if request.method == 'POST':
         form= TransacoesForm(request.POST)
         if form.is_valid():
-            form.save()
+            transacao= form.save()
+            if categoria:
+                transacao.categoria=categoria
+            transacao.save()
             return redirect('listar_transacoes')
     else:
-        form = TransacoesForm()
-    return render(request, 'transacoes/cadastrar_transacoes.html', {'form': form})
+        form = TransacoesForm(initial={'categoria': categoria})
+
+    return render(request, 'transacoes/cadastrar_transacoes.html', {'form': form , 'categoria_fixa': categoria})
 
 def transacoes_delete(request, pk):
     transacao = get_object_or_404(Transacoes, pk=pk)
@@ -24,4 +35,18 @@ def transacoes_delete(request, pk):
         return redirect('listar_transacoes')
     return redirect('listar_transacoes')
 
-    
+def edit_transacao(request, pk):
+    transacao = get_object_or_404(Transacoes, pk=pk)
+    categoria_fixa = transacao.categoria
+    if request.method == 'POST':
+        form = TransacoesForm(request.POST, instance=transacao)
+        if form.is_valid:
+            transacao_editada= form.save(commit=False)
+            transacao_editada.categoria= categoria_fixa
+            transacao_editada.save()
+            return redirect('listar_transacoes')
+    else:
+        form = TransacoesForm(instance=transacao)
+    return render(request, 'transacoes/edit_transacoes.html', {'form': form, 'categoria_fixa': categoria_fixa})
+
+
